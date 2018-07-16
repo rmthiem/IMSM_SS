2f08a1d07171ef92f03938da4e0cff027fbb2455
Rebecca North <rmthiem@ncsu.edu>
2018-07-16 17:04:13 -0400

    contact info

---
+++ contacts.R
@@ -0,0 +1,10 @@
+#### Group Contacts
+###################
+
+# Rebecca North - (513) 594-7410
+# Ashley Gannon - (561) 351-8189
+# Chixiang Chen - (717) 329-5667
+# Duwani Katumullage - (936) 668-8413
+# Miaoqi Li - (513) 501-9196 
+# Mengfei Liu - (919) 793-8616
+# Jialu Wang - (443) 525-5219
